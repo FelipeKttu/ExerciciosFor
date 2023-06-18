e2446c4d2515c89b8395f425f2555f571393b7f7
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:16:35 -0300

    Update #Exercicio3.py

--- #Exercicio3.py
+++ #Exercicio3.py
@@ -1,7 +1,8 @@
-for C in range (20):
+for c in range (20):
     N = int(input('Digite o número:'))
-    R = N % 2
-    if R == 0:
+    Resto = N % 2
+    
+    if Resto == 0:
         print('{} é Par'.format(N))
-    elif R != 0:
+    elif Resto != 0:
         print('{} é Impar'.format(N))
