17a0f6f4f4bc18ff5268b62dc8f37da550f46804
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:14:35 -0300

    Update #Exercicio1.py

--- #Exercicio1.py
+++ #Exercicio1.py
@@ -4,6 +4,7 @@ for C in range(5):
     V2 = float(input('Digite o segundo valor:'))
     S = V1 + V2
     print('A soma é {}'.format(S))
+    
     if V1 > V2:
         print("O primeiro valor é maior !!")
     elif V2 > V1:
