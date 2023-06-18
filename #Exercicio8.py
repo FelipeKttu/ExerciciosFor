503fc2957140a338b4e59ed730372d9414c11d97
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:39:13 -0300

    Update #Exercicio8.py

--- #Exercicio8.py
+++ #Exercicio8.py
@@ -1,10 +1,17 @@
 #Exercicio8
+
 lista = []
 contador = 0
+
 for c in range (1,7):
-  N = float(input('Digite um número:'))
-  lista.append(N)
+  
+  numero = float(input('Digite um número:'))
+  lista.append(numero)
+
   print(lista)
+
   contador += 1
-  print('Número {} Posição {} '.format(N,c-1))
+
+  print('Número {} Posição {} '.format(numero,c-1))
+
 print('a lista é {}'.format(lista))
